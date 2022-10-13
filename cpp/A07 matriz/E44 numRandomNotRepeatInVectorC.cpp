89e7568f4c43ada9ac404212bfeddfa9179f66e6
brianlewyn <brianlewyn@gmail.com>
2022-10-12 20:32:46 -0500

    Exercise E44 & E45

---
+++ cpp/A07 matriz/E44 numRandomNotRepeatInVectorC.cpp
@@ -0,0 +1,111 @@
+/* Brayan Mejía Mora
+
+Escribir un programa que haga lo siguiente:
+   1. Crear dos matrices (A y B) de tamaño n x m, el usuario deberá decir el tamaño. Verifica que n y m no sean iguales.
+   2. Llenar las matrices con números aleatorios
+   3. Mostrar las matrices
+   4. Guardar en un vector c, todos los números que están en a pero no están en b
+   5. Mostrar el vector
+
+Entrada: tamaño de la matriz
+Salida: matriz A y B, y el vector C  
+*/
+
+#include <iostream>
+#include <stdlib.h>  // nRandom
+#include <time.h>    // nRandom
+using namespace std;
+
+int main() {
+   int n=0, m=0;
+   bool flag=false;
+
+   cout<<"Programa que crea dos matrices y la muestra con números aleatorios, luego muestra un vector con todo los números que están en A pero no están en B\n\n";
+
+   // Solicitar el tamaño de las matrices, y comprobar que n!=m
+   do {
+      if (flag)
+         cout<<"\nEl número de filas y columnas debe ser distinto\n";
+      cout<<"Digite el número de filas: "; cin>>n;
+      cout<<"Digite el número de columnas: "; cin>>m;
+      flag=true;
+   } while (n==m);
+
+   // Crear la matriz A y B
+   int MatrizA[n][m]={}, MatrizB[n][m]={};
+
+   // Poder generar números aleatorios
+   srand(time(NULL));
+
+   // Llenar las matrices con números aleatorios
+   for (int i=0; i<n; i++) { // rows
+      for (int j=0; j<m; j++) { // columns
+         MatrizA[i][j] = 1+rand()%(100);
+         MatrizB[i][j] = 1+rand()%(100);
+      }
+   }
+
+   // Mostrar la matriz A
+   cout<<"\nMatriz A: \n";
+   for (int i=0; i<n; i++) { // rows
+      for (int j=0; j<m; j++) { // columns
+         cout<<"["<<MatrizA[i][j]<<"]";
+      }
+      cout<<endl;
+   }
+
+   // Mostrar la matriz B
+   cout<<"\nMatriz B: \n";
+   for (int i=0; i<n; i++) { // rows
+      for (int j=0; j<m; j++) { // columns
+         cout<<"["<<MatrizB[i][j]<<"]";
+      }
+      cout<<endl;
+   }
+
+   // Guardar en un vector C, todos los números que están en A pero no están en B
+   int contC=0, contD=0, VectorC[n*m]={}, VectorD[n*m]={};
+   flag = false;
+
+   // MatrizA[i][j]
+   for (int i=0; i<n; i++) {
+      for (int j=0; j<m; j++) {
+
+         // MatrizB[x][y]
+         for (int x=0; x<n; x++) {
+            for (int y=0; y<m; y++) {
+               if (MatrizA[i][j] == MatrizB[x][y])
+                  flag=true;
+            }
+         }
+
+         // Guardar en el Vector C los números no coincidentes de la matriz A con B
+         if (flag) {
+            VectorD[contD] = MatrizA[i][j];
+            flag=false;
+            contD++;
+         } else {
+            VectorC[contC] = MatrizA[i][j];
+            contC++;
+         }
+      }
+   }
+
+
+   // Mostrar el vector C, los números no coincidentes de la matriz A con B
+   cout<<"\n\nVector C: \n";
+   for (int i=0; i<n*m; i++) {
+      if (VectorC[i]!='\0')
+         cout<<"["<<VectorC[i]<<"]";
+   }
+
+   // Mostrar los números coincidentes de la matriz A y B
+   cout<<"\n\nVector D: \n";
+   for (int i=0; i<n*m; i++) {
+      if (VectorD[i]!='\0')
+         cout<<"["<<VectorD[i]<<"]";
+   }
+
+   cout<<endl<<endl;
+   return 0;
+}
---
+++ cpp/A07 matriz/E45 matrizToVectorNotRepeat.cpp
@@ -0,0 +1,79 @@
+/* Brayan Mejía Mora
+
+Escribir un programa que haga lo siguiente:
+   1. Crear una matriz de números enteros de tamaño 4 x 4
+   2. Leer los elementos de la matriz  desde teclado
+   3. Pasar todos los elementos a un vector, 
+   4. Mostrar el vector
+   5. En el vector buscar los números repetidos y sólo dejar uno, los demás elementos del vector se deberán recorrer. Ejemplo:
+      [1][7][28][25][9][7][3][14][27][7][14][8][85]
+      [1][7][28][25][9][3][14][27][8][85]
+   6. Mostrar el vector con los números sin repetir
+
+Entrada:
+Salida:
+*/
+
+#include <iostream>
+using namespace std;
+
+const int n=4;
+
+int main() {
+   int matriz[n][n]={};
+
+   cout<<"Progama que lee desde teclado una matriz de 4x4 y muestra un vector con números sin repetir\n\n";
+
+   // Almacenar los datos leídos en la matriz
+   for (int i=0; i<n; i++) { // rows
+      for (int j=0; j<n; j++) { // columns
+         cout<<"Introduce el dato de la posición ["<<i<<"]["<<j<<"]: ";
+         cin>>matriz[i][j];
+      }
+   }
+
+   // Almacenar en el vector los datos de la matriz
+   int cont=0, vector[n*n]={};
+   for (int i=0; i<n; i++) {
+      for (int j=0; j<n; j++) {
+         vector[cont] = matriz[i][j];
+         cont++;
+      }
+   }
+
+   // Mostrar el vector
+   cout<<"\nVector: \n";
+   for (int i=0; i<n*n; i++) {
+      cout<<"["<<vector[i]<<"]";
+   }
+
+   // Buscar los números repetidos en el vector y sólo dejar uno
+   int vectorAux[n*n]={}; cont=0;
+   bool flag=false;
+   for (int i=0; i<n*n; i++) {
+      for (int j=0; j<n*n; j++) {
+         if (vector[i] == vectorAux[j] && i!=j) {
+            flag = true;
+         }
+      }
+
+      // Almacenar los datos no repetidos
+      if (flag) {
+         flag=false;
+      } else {
+         vectorAux[cont] = vector[i];
+         cont++;
+      }
+   }
+
+
+   // Mostrar el vector con números sin repetir
+   cout<<"\n\nVector sin repetir: \n";
+   for (int i=0; i<n*n; i++) {
+      if (vectorAux[i]!='\0')
+         cout<<"["<<vectorAux[i]<<"]";
+   }
+
+   cout<<endl<<endl;
+   return 0;
+}
